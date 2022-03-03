2075a73cba5c8ced08dff714bcebc9dd7aeda6f3
12Aleks <tanais2010@yahoo.com>
2022-03-03 19:58:44 +0100

    In project were addede favicons

--- app/components/mainContainer.js
+++ app/components/mainContainer.js
@@ -10,7 +10,12 @@ const MainContainer = ({children, title, keywords}) => {
         <section className={selector.payload ? undefined: 'dark'}>
             <Head>
                 <title>{title}</title>
-                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+                <link rel="manifest" href="/static/site.webmanifest" />
+                <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="theme-color" content="#ffffff" />
                 <meta keywords={'pixem converter' + keywords}></meta>
             </Head>
             <Header />
