0e148c8ab3970406d40e98152c3afc1531720380
12Aleks <tanais2010@yahoo.com>
2022-03-17 20:45:24 +0100

    Description was added in project

--- app/components/MainContainer.js
+++ app/components/MainContainer.js
@@ -7,21 +7,22 @@ const MainContainer = ({children, title, keywords}) => {
     const selector = useSelector(state => state.colorTheme.value);
 
     return (
-        <section className={selector.payload ? undefined: 'dark'}>
+        <section className={selector.payload ? undefined : 'dark'}>
             <Head>
-                <title>{title}</title>
+                <title>Pixem | {title}</title>
                 <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-                <link rel="manifest" href="/static/site.webmanifest" />
-                <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
-                <meta name="description" content="Pixem app is an online tool that helps you convert pixels into different relative and absolute measurement units."/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
+                <link rel="manifest" href="/static/site.webmanifest"/>
+                <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="description"
+                      content="Pixem app is an online tool that helps you convert pixels into different relative and absolute measurement units."/>
                 <meta keywords={`pixem converter ${keywords}`}/>
                 <meta name="robots" content="noindex,nofollow"/>
-                <meta name="google-site-verification" content="1N41cNbyiMA6g6uIMYhMIBq-G8M_-ewOO3WGfZYefGw" />
+                <meta name="google-site-verification" content="1N41cNbyiMA6g6uIMYhMIBq-G8M_-ewOO3WGfZYefGw"/>
             </Head>
-            <Header />
-               {children}
+            <Header/>
+            {children}
             <Footer/>
         </section>
     );
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,51 @@
-import Home from "./home";
 import MainContainer from "../components/MainContainer";
+import {Tabs, Tab, Container, Row, Col} from 'react-bootstrap';
+import First from "../components/subpages/First";
+import Second from "../components/subpages/Second";
+import Third from "../components/subpages/Third";
+import Fourth from "../components/subpages/Fourth";
+import Fifth from "../components/subpages/Fifth";
 
 const Index = () => {
     return (
         <MainContainer title={'Home page'} keywords={'Home page'}>
-            <Home/>
+            <div className='main'>
+                <Tabs defaultActiveKey="first" id="uncontrolled-tab-example" className="mb-2">
+                    <Tab eventKey="first" title="Px to Rem">
+                        <First/>
+                    </Tab>
+                    <Tab eventKey="second" title="Px to Em">
+                        <Second/>
+                    </Tab>
+                    <Tab eventKey="third" title="Px to Percent">
+                        <Third/>
+                    </Tab>
+                    <Tab eventKey="fourth" title="Px to Pt">
+                        <Fourth/>
+                    </Tab>
+                    <Tab eventKey="fifth" title="Px to Centimeter">
+                        <Fifth/>
+                    </Tab>
+                </Tabs>
+                <Container className='app_description'>
+                    <Row>
+                        <Col sm={{span: 12, offset: 0}} md={{span: 10, offset: 1}} lg={{span: 10, offset: 1}}>
+                            <h4>Application usage</h4>
+                            <ol>
+                                <li>In order to use the application which convert pixels to other units of measurement, just
+                                    enter the desired number in the text window with the heading Px;</li>
+                                <li>You can also click on any number in the table, and it will be automatically applied;</li>
+                                <li>Unit sizes changes automatically
+                                    displayed in the column with the heading PREVIEW;</li>
+                                <li>The text window displays text that can be changed, below the text window
+                                    there are several selectors with which you can select and apply yours styles;</li>
+                                <li>In order to see the CSS code with the added styles, press the SHOW CODE button in the text
+                                    window next to the text.</li>
+                            </ol>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </MainContainer>
     );
 };
